0541d0a17be64dade72962f64cc6f412bb82a4f9
Bobby Joe Smith III <bsmith07@risd.edu>
2019-01-22 17:31:16 -0500

    Populate firstname from object to dom

--- 03_JavaScriptClasses/script.js
+++ 03_JavaScriptClasses/script.js
@@ -1,35 +1,53 @@
-// Objectify Myself
-// firstname
-// lastname
-// age
-// Major/Discipline
-// Places you've lived
-// method to create fullname
-// Have you seen the movie bird box
-// physicaly features: height, hair color, eye color, tattoos?, piercings?
+window.onload = function(e){
+  var body =  document.querySelector("body");
 
-var bobbyjoe = {
-  firstName: "Bobby Joe",
-  lastName: "Smith III",
-  age: 31,
-  major: ["Graphic Design", "Photo"],
-  places: [
-    {state: "Minnesota", city: ["St.Paul", "Minneapolis"]},
-    {state: "Maryland", city: "Baltimore"},
-    {state: "Rhode Island", city: "Providence"}
-  ],
-  features: {
-    height: "5'11",
-    hairColor: "black",
-    eyeColor: "beautiful brown",
-    tattoos: false,
-    piercings: true
-  },
-  seenBirdbox: false,
-  fullName: function() {
-    console.log(this.firstName + " " + this.lastName);
+
+
+  // Objectify Myself
+  // firstname
+  // lastname
+  // age
+  // Major/Discipline
+  // Places you've lived
+  // method to create fullname
+  // Have you seen the movie bird box
+  // physicaly features: height, hair color, eye color, tattoos?, piercings?
+
+  var bobbyjoe = {
+    firstName: "Bobby Joe",
+    lastName: "Smith III",
+    age: 31,
+    major: ["Graphic Design", "Photo"],
+    places: [
+      {state: "Minnesota", city: ["St.Paul", "Minneapolis"]},
+      {state: "Maryland", city: "Baltimore"},
+      {state: "Rhode Island", city: "Providence"}
+    ],
+    features: {
+      height: "5'11",
+      hairColor: "black",
+      eyeColor: "beautiful brown",
+      tattoos: false,
+      piercings: true
+    },
+    seenBirdbox: false,
+    fullName: function() {
+      console.log(this.firstName + " " + this.lastName);
+    }
   }
-}
 
-console.log(bobbyjoe.firstname);
-bobbyjoe.fullName();
+  console.log(bobbyjoe.firstname);
+  bobbyjoe.fullName();
+
+  // append data to DOM
+  // get body node from html
+  // create child node
+  function addInfo () {
+    var pNode = document.createElement("p");
+    var pText = document.createTextNode(`First Name: ${bobbyjoe.firstName}`);
+    pNode.appendChild(pText);
+    body.appendChild(pNode);
+  }
+
+  addInfo();
+}
